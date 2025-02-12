bc939a313b9d5801d253db2835a909a755cda395
Vincent Wong <115456837+vwdiy2u@users.noreply.github.com>
2025-02-12 22:05:04 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        console.log('Received:', message);
+        
+        // Relay message to all connected clients
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+console.log("WebSocket Server running on port 8080");
